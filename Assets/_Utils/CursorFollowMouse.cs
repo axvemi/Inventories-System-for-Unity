3f7afb8585d18dde21de9fb1497c169aaf5b9e93
axvemi <axvemi@gmail.com>
2022-01-11 01:41:59 +0100

    Added screen space support

--- Assets/_Utils/CursorFollowMouse.cs
+++ Assets/_Utils/CursorFollowMouse.cs
@@ -5,30 +5,38 @@ namespace Axvemi.ClassicInventory
 {
     public class CursorFollowMouse : MonoBehaviour
     {
-
-        [SerializeField] private RectTransform canvasRectTransform = null;
+        [SerializeField] private Canvas canvas = null;
+        private RectTransform canvasRectTransform = null;
         private RectTransform rectTransform = null;
 
         #region MONOBEHAVIOUR
 
         private void Awake() {
             rectTransform = GetComponent<RectTransform>();
+            canvasRectTransform = canvas.GetComponent<RectTransform>();
         }
         private void OnEnable() {
             Vector2 startingPosition = rectTransform.anchoredPosition;
         }
 
         private void Update() {
-            FollowMousePosition();
+            if(canvas.renderMode == RenderMode.ScreenSpaceCamera){
+                FollowMouseOnCameraSpace();
+            }
+            else if(canvas.renderMode == RenderMode.ScreenSpaceOverlay){
+                FollowMouseOnOverlaySpace();
+            }
         }
         #endregion
 
-
-        /// <summary>
-        /// Follows the mouse position
-        /// </summary>
-        private void FollowMousePosition(){
+        private void FollowMouseOnOverlaySpace(){
             rectTransform.anchoredPosition = Mouse.current.position.ReadValue() / canvasRectTransform.localScale.x;
+        }
+
+        private void FollowMouseOnCameraSpace(){
+            Vector2 canvasPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, Mouse.current.position.ReadValue(), canvas.worldCamera , out canvasPoint);
+            rectTransform.anchoredPosition = canvasPoint;
         }
     }
 }
